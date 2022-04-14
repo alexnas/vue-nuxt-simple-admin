03c516d38f87b4b9d066459d0b089f124cb4e1bc
Alexnander Nasokin <alexnas2python@gmail.com>
2022-04-14 23:19:44 +0300

    Make default layout. Make NavBar.

---
+++ components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <b-navbar toggleable="sm" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-collapse is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <main>
+      <b-container>
+        <nuxt />
+      </b-container>
+    </main>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
