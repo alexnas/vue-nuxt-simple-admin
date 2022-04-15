fcbf2adf69e17a4e2b20752d78c03f8f6e226e53
Alexnander Nasokin <alexnas2python@gmail.com>
2022-04-15 16:51:29 +0300

    Make login page in separate layout.

---
+++ layouts/empty.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <nuxt />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    flex-direction: columns;
+    padding-top: 5rem;
+}
+</style>
