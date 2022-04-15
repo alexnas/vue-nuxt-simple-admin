44033a147b6a98777059c6b23dbb6945c7f8f6a5
Alexnander Nasokin <alexnas2python@gmail.com>
2022-04-15 17:00:33 +0300

    Install axios module.

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,10 @@ export default {
     modules: [
         // https://go.nuxtjs.dev/bootstrap
         'bootstrap-vue/nuxt',
+        '@nuxtjs/axios',
     ],
+
+    axios: {},
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
