56d482a1b744fdbbe410d959a3465c6fcfd0256a
Alexnander Nasokin <alexnas2python@gmail.com>
2022-04-16 18:33:35 +0300

    Use Nuxt Fetch Hook to dispatch data through VUEX action.

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
         <h1>{{ pageTitle }}</h1>
+
         <ul>
             <li v-for="user in users" :key="user.id">
                 <a href="#" @click.prevent="goTo(user)"> User: {{ user.name }} ({{ user.email }})</a>
@@ -11,18 +12,24 @@
 
 <script>
 export default {
-    async asyncData({ store, error }) {
-        try {
-            const users = await store.dispatch('users/fetchUsers')
-            return { users }
-        } catch (err) {
-            error(err)
-        }
-    },
     data() {
         return {
             pageTitle: 'Users Page Content',
         }
+    },
+
+    async fetch({ store, error }) {
+        try {
+            await store.dispatch('users/fetchUsers')
+        } catch (err) {
+            error(err)
+        }
+    },
+
+    computed: {
+        users() {
+            return this.$store.getters['users/users']
+        },
     },
 
     methods: {
--- store/users.js
+++ store/users.js
@@ -1,10 +1,32 @@
+export const state = () => {
+    users: []
+}
+
+export const mutations = {
+    setUsers(state, users) {
+        state.users = users
+    },
+}
+
 export const actions = {
-    async fetchUsers() {
+    async fetchUsers({ commit }) {
         try {
             const users = await this.$axios.$get('https://jsonplaceholder.typicode.com/users')
-            return users
+            commit('setUsers', users)
         } catch (err) {
             throw err
         }
     },
+
+    async fetchUserById({}, userId) {
+        try {
+            return await this.$axios.$get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        } catch (err) {
+            throw err
+        }
+    },
+}
+
+export const getters = {
+    users: (state) => state.users,
 }
